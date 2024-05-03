3ba92f393b57d2d69224756038f0eca9c5843dd2
JAmwenya <jnehondo@gmail.com>
2024-05-03 21:14:20 +0000

    update gaerment guru

--- back_end/models/engine/file_storage.py
+++ back_end/models/engine/file_storage.py
@@ -1,49 +1,27 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for Garmrnt Guru"""
-import json
-
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from models.database import StoreImage
 
 class FileStorage:
-    """This class manages storage of Garment Guru models in JSON format"""
-    __file_path = 'file.json'
-    __objects = {}
+    def __init__(self):
+        # Connecting to the database
+        self.engine = create_engine('sqlite:///images.db')
+        self.Session = sessionmaker(bind=self.engine)
+        self.session = self.Session()
 
+    # Return all images from the database
     def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        return self.session.query(StoreImage).all()
 
-    def new(self, obj):
-        """Adds new object to storage dictionary"""
-        self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
+    # Adding a new image to the database
+    def new(self, image):
+        self.session.add(image)
+        self.session.commit()
 
+    # Committing changes to the database
     def save(self):
-        """Saves storage dictionary to file"""
-        with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            temp.update(FileStorage.__objects)
-            for key, val in temp.items():
-                temp[key] = val.to_dict()
-            json.dump(temp, f)
+        self.session.commit()
 
     def reload(self):
-        """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.image_upload import ImageUpload
-        from models.closet import Closet
-        from models.basket import Basket
-        from models.garment import Garment
-        from models.status import Status
-
-        classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Status': Status, 'Image_upload': ImageUpload,
-                    'Closet': Closet, 'Basket': Basket, 'Garment': Garment,
-                  }
-        try:
-            temp = {}
-            with open(FileStorage.__file_path, 'r') as f:
-                temp = json.load(f)
-                for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
-        except FileNotFoundError:
-            pass
+        pass
